3ddd8eef7e4caa877d8ba6b01fd0ce6a00226767
docfr10 <docfr10@yandex.ru>
2024-07-29 12:49:25 +0300

    Added LazyColumn container

---
+++ app/src/main/java/com/example/jetpack_compose_tutorial/ui/containers/LazyRow.kt
@@ -0,0 +1,86 @@
+package com.example.jetpack_compose_tutorial.ui.containers
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun BasicLazyRowExample() {
+    val itemsList = List(20) { "Item #$it" }
+    LazyRow {
+        items(itemsList) { item ->
+            Text(
+                text = item,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomLazyRowExample() {
+    val itemsList = List(20) { "Item #$it" }
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 16.dp), // Отступы вокруг списка
+        horizontalArrangement = Arrangement.spacedBy(10.dp) // Расстояние между элементами
+    ) {
+        items(itemsList) { item ->
+            Text(
+                text = item,
+                modifier = Modifier.padding(8.dp) // Внутренние отступы для каждого элемента
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LazyRowWithHeadersExample() {
+    val section1Items = List(5) { "Section 1 - Item #$it" }
+    val section2Items = List(5) { "Section 2 - Item #$it" }
+
+    LazyRow(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        item {
+            Text(
+                text = "Section 1",
+                modifier = Modifier.padding(horizontal = 8.dp),
+                style = androidx.compose.ui.text.TextStyle.Default.copy(
+                    fontSize = 20.sp
+                )
+            )
+        }
+        items(section1Items) { item ->
+            Text(
+                text = item,
+                modifier = Modifier.padding(horizontal = 4.dp)
+            )
+        }
+        item {
+            Text(
+                text = "Section 2",
+                modifier = Modifier.padding(horizontal = 8.dp),
+                style = androidx.compose.ui.text.TextStyle.Default.copy(
+                    fontSize = 20.sp
+                )
+            )
+        }
+        items(section2Items) { item ->
+            Text(
+                text = item,
+                modifier = Modifier.padding(horizontal = 4.dp)
+            )
+        }
+    }
+}
